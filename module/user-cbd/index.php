0f451b1658190ce96eb672dc457f66de4ac22b53
jakaprayudha <jakaprayudha3@gmail.com>
2024-08-28 14:30:53 +0700

    feat(cabdis) dashboard page

--- module/user-cbd/index.php
+++ module/user-cbd/index.php
@@ -1,8 +1,8 @@
 <?php
 $page = "Home";
 require '../../controller/view.php';
-$nik = $_SESSION['nik'];
-$checkprofile = mysqli_query($koneksi, "SELECT * FROM profile WHERE user_id='$nik'");
+$uid = $_SESSION['uid'];
+$checkprofile = mysqli_query($koneksi, "SELECT * FROM profile_operator_cabdis WHERE uid_user='$uid'");
 $dataprofile = mysqli_fetch_array($checkprofile);
 ?>
 <!DOCTYPE html>
@@ -13,6 +13,24 @@ $dataprofile = mysqli_fetch_array($checkprofile);
    <?php
    require '../../assets/template/head.php';
    ?>
+   <style>
+      .map-container {
+         position: relative;
+         overflow: hidden;
+         width: 100%;
+         padding-top: 56.25%;
+         /* 16:9 Aspect Ratio */
+      }
+
+      .map-container iframe {
+         position: absolute;
+         top: 0;
+         left: 0;
+         width: 100%;
+         height: 100%;
+         border: 0;
+      }
+   </style>
 
 </head>
 
@@ -56,58 +74,16 @@ $dataprofile = mysqli_fetch_array($checkprofile);
                               <li class="breadcrumb-item active">Profile</li>
                            </ol>
                         </div>
-                        <h4 class="page-title">Selamat Datang</h4>
+                        <h4 class="page-title">Selamat Datang, <?= $_SESSION['fullname'] ?></h4>
                      </div>
                   </div>
                </div>
                <!-- end page title -->
 
 
-               <div class="row">
-                  <div class="col-sm-12">
-                     <!-- Profile -->
-                     <div class="card bg-primary">
-                        <div class="card-body profile-user-box">
-
-                           <div class="row">
-                              <div class="col-sm-8">
-                                 <div class="media">
-                                    <span class="float-left m-2 mr-4"><img src="assets/images/users/avatar-2.jpg" style="height: 100px;" alt="" class="rounded-circle img-thumbnail"></span>
-                                    <div class="media-body">
-
-                                       <h4 class="mt-1 mb-1 text-white"><?= $dataprofile['nama'] ?></h4>
-                                       <p class="font-13 text-white-50">STMIK Triguna Dharma</p>
-
-                                       <ul class="mb-0 list-inline text-light">
-                                          <li class="list-inline-item mr-3">
-                                             <h5 class="mb-1">Strata 1</h5>
-                                             <p class="mb-0 font-13 text-white-50">Penempatan</p>
-                                          </li>
-                                          <li class="list-inline-item">
-                                             <h5 class="mb-1">Sistem Komputer</h5>
-                                             <p class="mb-0 font-13 text-white-50">Program Studi</p>
-                                          </li>
-                                       </ul>
-                                    </div> <!-- end media-body-->
-                                 </div>
-                              </div> <!-- end col-->
-                              <div class="col-sm-4">
-                                 <div class="text-center mt-sm-0 mt-3 text-sm-right">
-                                    <button type="button" class="btn btn-light">
-                                       <i class="mdi mdi-account-edit mr-1"></i> Edit Profile
-                                    </button>
-                                 </div>
-                              </div> <!-- end col-->
-                           </div> <!-- end row -->
-                        </div> <!-- end card-body/ profile-user-box-->
-                     </div><!--end profile/ card -->
-                  </div> <!-- end col-->
-               </div>
-               <!-- end row -->
-
-               <div class="alert alert-primary" role="alert">
-                  <strong> PENGAJUAN KENAIKAN JABATAN FUNGSIONAL SEGERA DIBUKA</strong>
-                  Pengajuan kenaikan jabatan fungsional (Lektor Kepala dan Guru Besar) akan dibuka melalui SISTER. Sebelum pengajuan kenaikan jabatan fungsional Guru dibuka, pastikan Guru sudah melengkapi Rumpun Ilmu, memutakhirkan data di profil SISTER dan Operator PAK untuk melengkapi dokumen persyaratan. Klik Info Selengkapnya untuk mempelajari lebih lanjut. Selain perihal di atas, kami juga mengimbau Guru dan Admin PT untuk melakukan pemadanan data dan Operator PAK untuk menggunakan perangkat bantu AK Konversi <span class="badge badge-primary">Selengkapnya</span>
+               <div class="alert alert-primary alert-info" role="alert">
+                  <i class="dripicons-information mr-2"></i>
+                  Lengkapi kebutuhan data master untuk Cabang Dinas Wilayah anda sebelum tanggal 30 Agustus 2024 23.00 WIB, SISTER DISDIK Dinas Pendidikan Provinsi Sumatera Utara akan secara otomatis melakukan sinkronisasi data pada tanggal yang telah ditentukan.
                </div>
                <div class="row">
                   <div class="col-lg-12">
@@ -179,47 +155,60 @@ $dataprofile = mysqli_fetch_array($checkprofile);
                            <div class="card tilebox-one">
                               <div class="card-body">
                                  <i class="dripicons-user float-right text-muted"></i>
-                                 <h6 class="text-muted text-uppercase mt-0">Data Pokok</h6>
+                                 <h6 class="text-muted text-uppercase mt-0">Data Cabang Dinas Pendidikan</h6>
                                  <p>
                                     Jika ingin mengubah data, silakan lakukan melalui menu terkait
                                  </p>
                                  <div class="form-group row mb-1">
-                                    <label for="inputEmail3" class="col-3 col-form-label">NIK</label>
+                                    <label for="inputEmail3" class="col-3 col-form-label">Kode</label>
                                     <div class="col-9">
-                                       <input type="text" readonly class="form-control-plaintext" id="example-static" value="1720120123123123">
+                                       <input type="text" readonly class="form-control-plaintext" id="example-static" value=":">
                                     </div>
                                  </div> <!-- end card-body-->
                                  <div class=" form-group row mb-1">
-                                    <label for="inputEmail3" class="col-3 col-form-label">Tanggal Lahir</label>
+                                    <label for="inputEmail3" class="col-3 col-form-label">Kepala Cabang Dinas</label>
                                     <div class="col-9">
-                                       <input type="text" readonly class="form-control-plaintext" id="example-static" value="20 Mei 1992">
+                                       <input type="text" readonly class="form-control-plaintext" id="example-static" value=":">
                                     </div>
                                  </div> <!-- end card-body-->
                                  <div class="form-group row mb-1">
-                                    <label for="inputEmail3" class="col-3 col-form-label">NIDN/NIDK</label>
+                                    <label for="inputEmail3" class="col-3 col-form-label">Alamat</label>
                                     <div class="col-9">
-                                       <input type="text" readonly class="form-control-plaintext" id="example-static" value="0120059201"> <span>
-                                          Tanggal mulai menjadi Guru: 22 Jun 2016
-                                       </span>
+                                       <input type="text" readonly class="form-control-plaintext" id="example-static" value=":">
                                     </div>
                                  </div> <!-- end card-body-->
                                  <div class="form-group row mb-1">
-                                    <label for="inputEmail3" class="col-3 col-form-label">NIP</label>
+                                    <label for="inputEmail3" class="col-3 col-form-label">No. Telepon</label>
                                     <div class="col-9">
-                                       <input type="text" readonly class="form-control-plaintext" id="example-static" value="-">
+                                       <input type="text" readonly class="form-control-plaintext" id="example-static" value=":">
                                     </div>
                                  </div> <!-- end card-body-->
                                  <div class="form-group row mb-1">
-                                    <label for="inputEmail3" class="col-3 col-form-label">Jabatan</label>
+                                    <label for="inputEmail3" class="col-3 col-form-label">Email</label>
                                     <div class="col-9">
-                                       <input type="text" readonly class="form-control-plaintext" id="example-static" value="Lektor"> <span>Tanggal mulai: 1 Sep 2020</span>
+                                       <input type="text" readonly class="form-control-plaintext" id="example-static" value=":">
                                     </div>
                                  </div> <!-- end card-body-->
 
                                  <div class="form-group row mb-1">
-                                    <label for="inputEmail3" class="col-3 col-form-label">Kepangkatan</label>
+                                    <label for="inputEmail3" class="col-3 col-form-label">Maps</label>
                                     <div class="col-9">
-                                       <input type="text" readonly class="form-control-plaintext" id="example-static" value="Penata - III/c"> <span>Tanggal mulai: 1 Jan 2021</span>
+                                       <!-- Portlet card -->
+                                       <div class="card mb-md-0 mb-3">
+                                          <div class="card-body">
+                                             <h5 class="card-title mb-0">Peta</h5>
+
+                                             <div id="cardCollpase1" class="collapse pt-3 show">
+                                                <div class="map-container">
+                                                   <iframe
+                                                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3153.363755008809!2d144.9560541153211!3d-37.81720997975143!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad642af0f11fd81%3A0xf577a8ef1a4a6c0!2sFederation%20Square%2C%20Melbourne%20VIC%203000%2C%20Australia!5e0!3m2!1sen!2sus!4v1600000000000!5m2!1sen!2sus"
+                                                      loading="lazy"
+                                                      allowfullscreen="">
+                                                   </iframe>
+                                                </div>
+                                             </div>
+                                          </div>
+                                       </div> <!-- end card-->
                                     </div>
                                  </div> <!-- end card-body-->
 
